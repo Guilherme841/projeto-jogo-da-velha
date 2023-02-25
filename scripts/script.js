f21f99f18b9760ac2e28877c0d706f471d7cb44c
Guilherme Oliveira <116916546+Guilherme841@users.noreply.github.com>
2023-02-25 18:25:59 -0300

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,28 @@
+const blocos = document.querySelectorAll(".blocos");
+const xis = document.getElementById("xispng");
+const bola = document.getElementById("bolapng");
+// let execução = false;
+
+xis.addEventListener("click", function() {
+    
+})
+
+bola.addEventListener("click", function() {
+    
+})
+
+for (let bloco of blocos) {
+  bloco.addEventListener("click", async function() {
+    // if (execução) {
+    //     return;
+    //   }
+    //   execução = true;
+    let img = document.createElement("img");
+    img.src = "../imagens/Xis.png";
+    bloco.appendChild(img);
+    img.style.display = "flex";
+    img.style.margin = "auto";
+    img.style.alignItems = "center";
+    img.style.justifyContent = "center";
+  });
+}
