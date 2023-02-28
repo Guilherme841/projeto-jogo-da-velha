f344cf36e4a5e8ba13ed4410621c47033ee3465c
Guilherme Oliveira <116916546+Guilherme841@users.noreply.github.com>
2023-02-28 19:38:18 -0300

    Update

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,74 @@
 const blocos = document.querySelectorAll(".blocos");
 const xis = document.getElementById("xispng");
 const bola = document.getElementById("bolapng");
-// let execução = false;
+var execução = false;
+var tipo = "";
 
-xis.addEventListener("click", function () {});
+xis.addEventListener("click", function () {
+  if (execução) {
+    return;
+  }
+  tipo = "xis";
+});
 
-bola.addEventListener("click", function () {});
+bola.addEventListener("click", function () {
+  if (execução) {
+    return;
+  }
+  tipo = "bola";
+});
 
-for (let bloco of blocos) {
-  bloco.addEventListener("click", async function () {
+for (var bloco of blocos) {
+  bloco.addEventListener("click",function() {
     // if (execução) {
     //     return;
     //   }
-    //   execução = true;
+    if (tipo === "") {
+      return;
+    }
+    execução = true;
+    if (tipo === "xis") {
+      let img = document.createElement("img");
+      img.src = "../imagens/Xis.png";
+      this.appendChild(img);
+      
+      img.style.display = "flex";
+      img.style.margin = "auto";
+      img.style.alignItems = "center";
+      img.style.justifyContent = "center";
+    } else if (tipo === "bola") {
+      let img = document.createElement("img");
+      img.src = "../imagens/Circle.png";
+      this.appendChild(img);
+      img.style.display = "flex";
+      img.style.margin = "auto";
+      img.style.alignItems = "center";
+      img.style.justifyContent = "center";
+    }
+    site();
+  });
+}
+
+function site() {
+  const arrQuadros = Array.from(blocos);
+  const quadroAleatorio =
+    arrQuadros[Math.floor(Math.random() * arrQuadros.length)];
+    console.log(quadroAleatorio)
+  if (tipo !== "bola") {
     let img = document.createElement("img");
-    img.src = "../imagens/Xis.png";
-    bloco.appendChild(img);
+    img.src = "../imagens/Circle.png";
+    quadroAleatorio.appendChild(img);
     img.style.display = "flex";
     img.style.margin = "auto";
     img.style.alignItems = "center";
     img.style.justifyContent = "center";
-  });
+  } else {
+    let img = document.createElement("img");
+    img.src = "../imagens/Xis.png";
+    quadroAleatorio.appendChild(img);
+    img.style.display = "flex";
+    img.style.margin = "auto";
+    img.style.alignItems = "center";
+    img.style.justifyContent = "center";
+  }
 }
