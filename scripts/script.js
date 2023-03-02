0ebc41194c9bfb5860bec3323ea149310939a392
Guilherme Oliveira <116916546+Guilherme841@users.noreply.github.com>
2023-03-02 18:54:14 -0300

    Update

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,17 @@
 const blocos = document.querySelectorAll(".blocos");
 const xis = document.getElementById("xispng");
 const bola = document.getElementById("bolapng");
+
 var execução = false;
 var tipo = "";
+
+let pointx = localStorage.getItem("placarx");
+placarx.textContent = pointx;
+
+let plaBall = localStorage.getItem("boardBola");
+if (!isNaN(plaBall)) {
+  boardBola.textContent = parseInt(plaBall);
+}
 
 xis.addEventListener("click", function () {
   if (execução) {
@@ -23,19 +32,59 @@ for (var bloco of blocos) {
     // if (execução) {
     //     return;
     //   }
-    let jogadas = document.querySelectorAll(".jogado");
-    if (jogadas.length > 3) {
-      location.reload();
-    }
     if (this.classList.contains("jogado")) {
+      execução = false;
       return;
     } else if (this.classList.contains("jogado2")) {
+      execução = false;
       return;
     }
     if (tipo === "") {
+      execução = false;
       return;
     }
     execução = true;
+    let jogadas = document.querySelectorAll(".jogado");
+    let arrJogadas = Array.from(jogadas);
+    if (arrJogadas.length === 4) {
+      location.reload();
+    }
+    let matriz = [
+      ["div1", "div2", "div3"],
+      ["div1", "div4", "div7"],
+      ["div1", "div5", "div9"],
+      ["div3", "div5", "div7"],
+      ["div2", "div5", "div8"],
+      ["div3", "div6", "div9"],
+      ["div4", "div5", "div6"],
+      ["div7", "div8", "div9"],
+    ];
+    for (let i = 0; i < matriz.length; i++) {
+      const divs = matriz[i];
+      let resultado = true;
+      for (let i2 = 0; i2 < divs.length; i2++) {
+        const divsId = document.getElementById(divs[i2]);
+        if (!divsId.classList.contains("jogado")) {
+          resultado = false;
+          break;
+        }
+      }
+      if (resultado) {
+        if (tipo === "xis") {
+          const placarx = document.getElementById("placarx");
+          let valorAtual = parseInt(placarx.textContent);
+          placarx.textContent = valorAtual + 1;
+          localStorage.setItem("placarx", valorAtual + 1);
+        } else if (tipo === "bola") {
+          const boardBola = document.getElementById("boardBola");
+          let valorAtualBall = parseInt(boardBola.textContent);
+          boardBola.textContent = valorAtualBall + 1;
+          localStorage.setItem("boardBola", valorAtualBall + 1);
+        }
+        location.reload();
+        return;
+      }
+    }
     if (tipo === "xis") {
       let img = document.createElement("img");
       img.src = "../imagens/Xis.png";
@@ -73,7 +122,7 @@ function site() {
   ) {
     quadroAleatorio = arrQuadros[Math.floor(Math.random() * arrQuadros.length)];
   }
-  if (tipo !== "bola") { 
+  if (tipo !== "bola") {
     let img = document.createElement("img");
     img.src = "../imagens/Circle.png";
     quadroAleatorio.appendChild(img);
